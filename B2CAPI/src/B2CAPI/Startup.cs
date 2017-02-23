3ce299e66b14b6990965253456307d11f684f8d5
Sendhil Kumar Gurunathan <sendhilkumarg@users.noreply.github.com>
2017-02-23 14:43:06 +1100

    Initial checkin

---
+++ B2CAPI/src/B2CAPI/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace B2CAPI
+{
+    public class Startup
+    {
+        public static string SignUpPolicyId;
+        public static string SignInPolicyId;
+        public static string ProfilePolicyId;
+        public static string ClientId;
+        public static string AadInstance;
+        public static string Tenant;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            // Add Authentication services.
+            services.AddAuthentication(
+                sharedOptions => sharedOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // Configure the OWIN pipeline to use cookie auth.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            // App config settings
+            ClientId = Configuration["AzureAD:ClientId"];
+            AadInstance = Configuration["AzureAD:AadInstance"];
+            Tenant = Configuration["AzureAD:Tenant"];
+
+            // B2C policy identifiers
+            SignInPolicyId = Configuration["AzureAD:SignInPolicyId"];
+
+
+            var options = new JwtBearerOptions
+            {
+                MetadataAddress = string.Format(AadInstance, Tenant, SignInPolicyId),
+                AuthenticationScheme = SignInPolicyId,
+                Audience = ClientId,
+                AutomaticAuthenticate = true,
+                RequireHttpsMetadata = false,
+                AutomaticChallenge = false,
+                IncludeErrorDetails = true,
+                Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        context.HttpContext.Items["jwt-workaround"] = null;
+
+                        return Task.FromResult(0);
+                    }
+                }
+            };
+            app.UseJwtBearerAuthentication(options);
+            // Configure MVC routes
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ B2CAPI/src/B2CWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B2CWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var apiurl = @"http://localhost:14111/api/values";
+
+            string returnFromFirstLayer = string.Empty;
+            string returnedFromSecondLayer = string.Empty;
+            string token = string.Empty;
+            try
+            {
+                returnFromFirstLayer = $"This is returned by CrmCallerFirstLayerController @ {DateTime.Now.ToString("dd-MMM-yyyy hh:mm:ss")}";
+                string webApiUrl = @"http://localhost:14111/api/values";
+
+                
+
+                token = Request.Headers["Authorization"];
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,
+                    webApiUrl);
+               // request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.Replace("Bearer", ""));
+/*                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    returnedFromSecondLayer = "[SUCCESS]  " + responseContent;
+                }
+                else
+                {
+                    returnedFromSecondLayer = "[FAILED]  " + response.StatusCode;
+                }*/
+               // string at = await GetAccessToken();
+
+
+            }
+            catch (Exception ex)
+            {
+                returnedFromSecondLayer = ex.Message + "|" + ex.StackTrace;
+            }
+
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = String.Format("Claims available for the user {0}", (User.FindFirst("name")?.Value));
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> ReadFromAPI()
+        {
+
+            var result = string.Empty;
+            try
+            {
+                var webApiUrl = @"http://localhost:14111/api/values";
+                var token = await HttpContext.Authentication.GetTokenAsync("id_token");
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                    webApiUrl);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token?.Replace("Bearer", ""));
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    result = "[SUCCESS]  " + responseContent;
+                }
+                else
+                {
+                    result = "[FAILED]  " + response.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message + "|" + ex.StackTrace;
+            }
+
+
+            return View("ReadFromAPI",result);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
